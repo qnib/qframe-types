9bf14c2dc314943a27d0948e4fd02a590d0f33ec
Christian Kniep <christian@qnib.org>
2017-04-18 15:09:24 +0200

    define type of plugin

--- plugins.go
+++ plugins.go
@@ -2,15 +2,17 @@ package qtypes
 
 import (
 	"log"
-	"github.com/zpatrick/go-config"
+	"fmt"
 	"strings"
+	"github.com/zpatrick/go-config"
 )
 
 type Plugin struct {
-	QChan QChan
-	Cfg config.Config
+	QChan 	QChan
+	Cfg 	config.Config
+	Typ		string
 	Version string
-	Name string
+	Name 	string
 }
 
 func NewPlugin(qChan QChan, cfg config.Config) Plugin {
@@ -38,6 +40,11 @@ func logStrToInt(level string) int {
 	}
 }
 
+func (p *Plugin) CfgStringOr(path, alt string) string {
+	res, _ := p.Cfg.StringOr(fmt.Sprintf("%s.%s.host", p.Typ, p.Name), alt)
+	return res
+}
+
 func (p *Plugin) Log(logLevel, msg string) {
 	dL, _ := p.Cfg.StringOr("log.level", "info")
 	dI := logStrToInt(dL)
@@ -47,11 +54,12 @@ func (p *Plugin) Log(logLevel, msg string) {
 	}
 }
 
-func NewNamedPlugin(qChan QChan, cfg config.Config, name, version string) Plugin {
+func NewNamedPlugin(qChan QChan, cfg config.Config, typ, name, version string) Plugin {
 	p := Plugin{
 		QChan: qChan,
 		Cfg:   cfg,
 	}
+	p.Typ = typ
 	p.Version = version
 	p.Name = name
 	return p
