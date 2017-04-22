c300bc092733cc8d629e8426f8ac5cc94a084127
Christian Kniep <christian@qnib.org>
2017-04-22 16:06:49 +0200

    add metrics types

--- channel.go
+++ channel.go
@@ -10,6 +10,7 @@ import (
 // QChan holds the broadcast channels to communicate
 type QChan struct {
 	Data *bcast.Group
+	Back *bcast.Group
 	Tick *bcast.Group
 	Done chan os.Signal
 }
@@ -18,13 +19,15 @@ type QChan struct {
 func NewQChan() QChan {
 	return QChan{
 		Data: bcast.NewGroup(), // create broadcast group
+		Back: bcast.NewGroup(), // create broadcast group
 		Tick: bcast.NewGroup(), // create broadcast group
 		Done: make(chan os.Signal, 1),
 	}
 }
 
 func (qc *QChan) Broadcast() {
-	log.Println("[II] Dispatch broadcast for Data and Tick")
+	log.Println("[II] Dispatch broadcast for Back, Data and Tick")
 	go qc.Data.Broadcast(0)
+	go qc.Back.Broadcast(0)
 	go qc.Tick.Broadcast(0)
 }
