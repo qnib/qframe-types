5796c2bb93823bc5ceeeeeb67f8ced89f10e231e
Christian Kniep <christian@qnib.org>
2017-04-26 09:54:18 +0200

    add new function GetCntByIP and SetCnt

--- containerinventory.go
+++ containerinventory.go
@@ -63,6 +63,47 @@ func (ci *ContainerInventory) GetCntByID(id string) (cnt types.ContainerJSON, er
 	return cnt, err
 }
 
+func (ci *ContainerInventory) GetCntByIP(ip string) (cnt types.ContainerJSON, err error) {
+	for id, v := range ci.IDtoIP {
+		if ip == v {
+			return ci.GetCntByID(id)
+		}
+
+	}
+	return cnt, err
+}
+
+func (ci *ContainerInventory) SetCntByEvent(event events.Message) (err error) {
+	id := event.Actor.ID
+	if event.Type != "container" {
+		return
+	}
+	switch event.Action {
+	case "die", "destroy":
+		if _, ok := ci.IDtoIP[id]; ok {
+			delete(ci.IDtoIP, id)
+		}
+		if _, ok := ci.Data[id]; ok {
+			delete(ci.Data, id)
+		}
+		return
+	case "start":
+		cnt, err := ci.Client.ContainerInspect(context.Background(), id)
+		if err != nil {
+			return err
+		}
+		ci.Data[id] = cnt
+		if cnt.State.Running {
+			for _, v := range cnt.NetworkSettings.Networks {
+				ci.IDtoIP[id] = v.IPAddress
+			}
+		} else {
+			fmt.Printf("cnt.State: %v\n", cnt.State)
+		}
+	}
+	return err
+}
+
 func (ci *ContainerInventory) GetCntByEvent(event events.Message) (cnt types.ContainerJSON, err error) {
 	id := event.Actor.ID
 	if event.Type != "container" {
