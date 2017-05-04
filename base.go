386b6c925e7d4051f251286c317b5a5c49164adb
Christian Kniep <christian@qnib.org>
2017-05-04 16:14:52 +0200

    Bump version: 0.3.1 → 0.3.2

--- base.go
+++ base.go
@@ -5,7 +5,7 @@ import (
 )
 
 const (
-	version = "0.3.1"
+	version = "0.3.2"
 )
 
 type Base struct {
--- main.go
+++ main.go
@@ -25,7 +25,7 @@ type QMsg struct {
 func NewQMsg(typ, source string) QMsg {
 	now := time.Now()
 	return QMsg{
-		QmsgVersion: 	"0.3.1",
+		QmsgVersion: 	"0.3.2",
 		Type:        	typ,
 		Level:       	6,
 		Source:      	source,
