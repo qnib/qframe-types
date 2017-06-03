2a9d1112826cf1b145aaa3c75cbbf2278dbe7e10
Christian Kniep <christian@qnib.org>
2017-06-03 18:29:16 +0200

    enhance StatsdPackets to support StatsQ

--- dimensions_test.go
+++ dimensions_test.go
@@ -13,3 +13,26 @@ func TestDimensions_String(t *testing.T) {
 	d.Add("key2", "val2")
 	assert.Equal(t,"key1=val1,key2=val2", d.String())
 }
+
+func TestNewDimensionsPre(t *testing.T) {
+	d := NewDimensionsPre(map[string]string{"key1": "val1"})
+	assert.Equal(t,"key1=val1", d.String())
+}
+
+func TestNewDimensionsFromString(t *testing.T) {
+	d := NewDimensionsFromString("")
+	assert.Equal(t,"", d.String())
+	d = NewDimensionsFromString("key1=val1")
+	assert.Equal(t,"key1=val1", d.String())
+	d = NewDimensionsFromString("key1=val1,val2=val2")
+	assert.Equal(t,"key1=val1,val2=val2", d.String())
+}
+
+func TestNewDimensionsFromBytes(t *testing.T) {
+	d := NewDimensionsFromBytes([]byte(""))
+	assert.Equal(t,"", d.String())
+	d = NewDimensionsFromBytes([]byte("key1=val1"))
+	assert.Equal(t,"key1=val1", d.String())
+	d = NewDimensionsFromBytes([]byte("key1=val1,val2=val2"))
+	assert.Equal(t,"key1=val1,val2=val2", d.String())
+}
--- statsdpacket.go
+++ statsdpacket.go
@@ -1,5 +1,11 @@
 package qtypes
 
+import (
+	"strings"
+	"crypto/sha1"
+	"encoding/hex"
+	"strconv"
+)
 
 type StatsdPacket struct {
 	Bucket   	string
@@ -7,19 +13,66 @@ type StatsdPacket struct {
 	ValStr   	string
 	Modifier 	string
 	Sampling 	float32
-	Group		Dimensions
 	Dimensions	Dimensions
 }
 
-func NewStatsdPacket(bucket string, val float64, modifier string) *StatsdPacket {
+type Packet struct {
+	Bucket   string
+	ValFlt   float64
+	ValStr   string
+	Modifier string
+	Sampling float32
+}
+
+func (sd *StatsdPacket) GetBucketKey() string {
+	return sd.Bucket
+}
+
+func (sd *StatsdPacket) GetDims() map[string]string {
+	return sd.Dimensions.GetDims()
+}
+
+func (sd *StatsdPacket) GenerateID() string {
+	idRaw := []string{sd.Bucket}
+	for key, val := range sd.Dimensions.Map {
+		idRaw = append(idRaw, key+"="+val)
+	}
+	s := strings.Join(idRaw, "_")
+	h := sha1.New()
+	h.Write([]byte(s))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+
+func NewStatsdPacket(bucket string, val string, modifier string) *StatsdPacket {
+	return NewStatsdPacketDims(bucket, val, modifier, NewDimensions())
+}
+
+func NewStatsdPacketDims(bucket string, val string, modifier string, dims Dimensions) *StatsdPacket {
+	vStr, vFlt, _ := ParseValStr(val)
 	return &StatsdPacket{
 		Bucket: bucket,
-		ValFlt: val,
+		ValFlt: vFlt,
+		ValStr: vStr,
 		Modifier: modifier,
 		Sampling: float32(1),
-		Group: NewDimensions(),
-		Dimensions: NewDimensions(),
+		Dimensions: dims,
 	}
+}
+
+func ParseValStr(s string) (str string, flt float64, err error) {
+	if strings.HasPrefix(s, "+") {
+		str = "+"
+		s = s[1:]
+	} else if strings.HasPrefix(s, "-") {
+		str = "-"
+		s = s[1:]
+	}
+	flt, err = strconv.ParseFloat(s, 64)
+	if err != nil {
+		return
+	}
+	return
 }
 
 func (sd *StatsdPacket) AddDimension(key, val string) {
